d584697f080458f285e1f85a8eba429cfa300068
mandalvikash <146217029+mandalvikash@users.noreply.github.com>
2024-03-12 17:37:48 +0000

    container config done

---
+++ 01_Basics/02_dataType.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3) // we are using nodejs not browser
+//code rediability  should be high
+console.log(3+4)
+console.log("Hitesh")
+
+
+let name="vikash"
+let age=18
+let isLoged=true
+
+//number=> 2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined
+//symbol=>unique
+
