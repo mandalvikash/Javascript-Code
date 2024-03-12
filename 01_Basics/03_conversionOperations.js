dbed2f163c1ecf0ee602b6dc1531490e94be04c2
mandalvikash <146217029+mandalvikash@users.noreply.github.com>
2024-03-12 18:02:56 +0000

    container config done

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,22 @@
+let score=33
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let s="21"
+let valInNumber=Number(s)
+console.log(typeof(valInNumber))
+console.log(valInNumber)
+
+// "33"=>33
+//"33abc"=>NaN
+//true=>1;false=>0
+
+let isLog=1
+let boolIsLog=Boolean(isLog)
+console.log(boolIsLog)
+
+let num=88
+let stringNum=String(num)
+console.log(stringNum)
+console.log(typeof stringNum)
