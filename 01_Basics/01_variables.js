70c358443f0d61983631b7db9df465efea7cd593
mandalvikash <146217029+mandalvikash@users.noreply.github.com>
2024-03-12 17:27:38 +0000

    container config done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId=14453;
+let accountEmail="hitesh@gooogle.com"
+var accountPassword="1234"
+accountCity="Jaipur"
+//nodaccountId=2;
+/*you cant cahnge the const variable value*/
+console.log(accountId);
+console.table([accountId,accountPassword,accountEmail]);
+
+/*prefer not to use car because of issue in block scope and functional scope */
