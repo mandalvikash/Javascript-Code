d71f2d35879ec17f2f28c4b96dadaf98f9df5212
mandalvikash <146217029+mandalvikash@users.noreply.github.com>
2024-03-20 06:03:32 +0000

    container config done

---
+++ 02_Basics/04_Object.js
@@ -0,0 +1,78 @@
+const tinderUser1=new Object()//Single ton object
+console.log(tinderUser1) //{}Object
+
+const tinderUser={}//Non single ton object
+console.log(tinderUser)//{} object
+
+ tinderUser.id="123abc"
+ tinderUser.name="Sammy"
+ tinderUser.isLoggedIn=false
+console.log(tinderUser)//{ id: '123abc', name: 'Sammy', isLoggedIn: false }
+
+const regularUser={
+    email:"some@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"Vikash",
+            lastname:"Mandal"
+        }
+    }
+}
+console.log(regularUser.fullname.userfullname)//{ firstname: 'Vikash', lastname: 'Mandal' }
+
+
+const obj1={
+    1:"a",
+    2:'b',
+    3:"c"
+}
+const obj2={
+    4:"d",
+    5:'e',
+    6:"f"
+}
+
+const obj3={obj1,obj2}
+console.log(obj3)//Object inside the object
+/*{
+  obj1: { '1': 'a', '2': 'b', '3': 'c' },
+  obj2: { '4': 'd', '5': 'e', '6': 'f' }
+} */
+
+const obj4=Object.assign(obj1,obj2)
+console.log(obj4) //{ '1': 'a', '2': 'b', '3': 'c', '4': 'd', '5': 'e', '6': 'f' }
+
+const obj5=Object.assign({},obj1,obj2)
+console.log(obj5)//{ '1': 'a', '2': 'b', '3': 'c', '4': 'd', '5': 'e', '6': 'f' }
+
+
+const obj6={...obj1,...obj2}
+console.log(obj6)//{ '1': 'a', '2': 'b', '3': 'c', '4': 'd', '5': 'e', '6': 'f' }
+
+
+const user2=[
+    {
+        id:1,
+        email:"1stgmail"
+    },
+    {
+        id:2,
+        email:"2ndgmail"
+    },
+    {
+        id:3,
+        email:"3rdgmail"
+    }
+]
+console.log(user2[0].email)//1stgmail
+console.log(user2[0].id)//1
+
+
+console.log(tinderUser)//{ id: '123abc', name: 'Sammy', isLoggedIn: false }
+console.log(Object.keys(tinderUser))//[ 'id', 'name', 'isLoggedIn' ]
+console.log(Object.values(tinderUser))//[ '123abc', 'Sammy', false ]
+
+//Object inside the object
+console.log(Object.entries(tinderUser))//[ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ]
+
+console.log(tinderUser.hasOwnProperty("isLoggedIn"))//true
