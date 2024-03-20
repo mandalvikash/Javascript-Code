03c8a9919e234509f32f6af4e6fb72b100199a7a
mandalvikash <146217029+mandalvikash@users.noreply.github.com>
2024-03-20 05:17:52 +0000

    container config done

--- 02_Basics/03_Object.js
+++ 02_Basics/03_Object.js
@@ -39,3 +39,38 @@ console.log( jsUser["mySym1"]) //key2 IS symbol  string type
 
 console.log(jsUser[mySym])//This is symbol
 
+
+console.log(jsUser)
+jsUser.email="hitesh@chatgpt.com"
+console.log(jsUser)
+
+//after freezeing no change will occur in the user
+Object.freeze(jsUser)
+jsUser.email="hitesh@microsoft.com"
+console.log(jsUser)
+
+
+const jsUser1={
+    name:"Hitesh",
+    age:18,
+    location:"Jaipur",
+    email:"hitesh@google.com",
+    isLoggedIn:false,
+    lastLoginDays:["Monday","Saturday"],
+
+    other:"Only one way to acces",
+    [mySym]:"This is symbol",
+    mySym1:" key2 IS symbol"
+}
+jsUser1.greeting=function(){
+    console.log(`Hello JS User`)
+}
+jsUser1.greetingtwo=function(){
+    console.log(`Hello JS User,${this.name}`)
+}
+
+console.log(jsUser1.greeting)//[Function (anonymous)]
+
+ jsUser1.greeting       //No ouput
+ jsUser1.greeting()     //Hello JS User
+ jsUser1.greetingtwo()  //Hello JS User,Hitesh 
