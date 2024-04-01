9313a80b5a48735dbfd113140628ea483e84de86
mandalvikash <146217029+mandalvikash@users.noreply.github.com>
2024-04-01 08:06:03 +0000

    container config done

---
+++ 03Extra_Executions/01_creation_execution.js
@@ -0,0 +1,10 @@
+let val1=19
+let val2=4
+function addNum(num1,num2){
+    let total=num1+num2
+    return total
+}
+let res1=addNum(val1,val2)
+console.log(res1)
+let res2=addNum(12,32)
+console.log(res2)
