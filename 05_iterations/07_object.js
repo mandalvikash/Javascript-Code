af3d1e3382fcbe2338b49f131d865698c64ad35f
mandalvikash <146217029+mandalvikash@users.noreply.github.com>
2024-04-02 18:32:05 +0000

    container config done

---
+++ 05_iterations/07_object.js
@@ -0,0 +1,19 @@
+const myCoding=[
+    {
+        languageName:"Javascript",
+        languageFileName:"js"
+    },
+    {
+        languageName:"Java",
+        languageFileName:"java"
+    },
+    {
+        languageName:"Python",
+        languageFileName:"py"
+    }
+]
+
+myCoding.forEach((item)=>{
+    console.log(item.languageName)
+    console.log(item.languageFileName)
+})
