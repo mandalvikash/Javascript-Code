398239a2ffcf11b737b49e06a27303dff89b6daf
mandalvikash <146217029+mandalvikash@users.noreply.github.com>
2024-04-02 17:41:22 +0000

    conatiner confi8g done

---
+++ 05_iterations/03_while_doWhile.js
@@ -0,0 +1,22 @@
+//while
+
+//Infinite
+// while(2==2){
+//     console.log("Hello");
+
+// }
+
+
+// let i=0
+// while(i<=10){
+//     console.log(`value of index is ${i}`)
+//     i++;
+// }
+
+//dowhile loop
+
+let j=0
+do{
+    console.log(`Score is ${j}`)
+    j++
+}while(j<=10);
